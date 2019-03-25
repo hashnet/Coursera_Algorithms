58a56af7efe2f4a917c01fc11419098c70b3ef17
hashnet <maidul.hasan@gmail.com>
2019-03-26 02:40:18 +1100

    WordNet more files

--- WordNet/src/SAP.java
+++ WordNet/src/SAP.java
@@ -1,48 +1,142 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
-
+	private Digraph G;
+	
 	// constructor takes a digraph (not necessarily a DAG)
 	public SAP(Digraph G) {
+		this.G = G;
 	}
 
 	// length of shortest ancestral path between v and w; -1 if no such path
 	public int length(int v, int w) {
-		return 0;
+		List<Integer> vL = new ArrayList<>();
+		vL.add(v);
+		
+		List<Integer> wL = new ArrayList<>();
+		wL.add(w);
+		
+		return length(vL, wL);
 	}
 
 	// a common ancestor of v and w that participates in a shortest ancestral
 	// path; -1 if no such path
 	public int ancestor(int v, int w) {
-		return 0;
+		List<Integer> vL = new ArrayList<>();
+		vL.add(v);
+		
+		List<Integer> wL = new ArrayList<>();
+		wL.add(w);
+		
+		return ancestor(vL, wL);
 	}
 
 	// length of shortest ancestral path between any vertex in v and any vertex
 	// in w; -1 if no such path
 	public int length(Iterable<Integer> v, Iterable<Integer> w) {
-		return 0;
+		return lengthAncestor(v, w).getV();
 	}
 
 	// a common ancestor that participates in shortest ancestral path; -1 if no
 	// such path
 	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-		return 0;
+		return lengthAncestor(v, w).getW();
+	}
+	
+	private Pair<Integer> lengthAncestor(Iterable<Integer> v, Iterable<Integer> w) {
+		boolean[] vMarked = new boolean[G.V()];
+		int[] vDist = new int[G.V()];
+		Queue<Integer> vQ = new ArrayDeque<>();
+		
+		boolean[] wMarked = new boolean[G.V()];
+		int[] wDist = new int[G.V()];
+		Queue<Integer> wQ = new ArrayDeque<>();
+		
+		for(int i : v) {
+			vMarked[i] = true;
+			vDist[i] = 0;
+			vQ.offer(i);
+		}
+		
+		for(int j : w) {
+			wMarked[j] = true;
+			wDist[j] = 0;
+			wQ.offer(j);
+		}
+		
+		boolean isV = true;
+		while(!vQ.isEmpty() || !wQ.isEmpty()) {
+			if(vQ.isEmpty()) isV = false;
+			else if(wQ.isEmpty()) isV = true;
+			
+			int x = isV ? vQ.poll() : wQ.poll();
+			if(vMarked[x] == true && wMarked[x] == true) {
+				return new Pair<Integer>(vDist[x] + wDist[x], x);
+			}
+			
+			for(int next : G.adj(x)) {
+				if((isV && !vMarked[next]) || (!isV && !wMarked[next]) ) {
+					if(isV) {
+						vMarked[next] = true;
+						vDist[next] = vDist[x] + 1;
+						vQ.offer(next);
+					} else {
+						wMarked[next] = true;
+						wDist[next] = wDist[x] + 1;
+						wQ.offer(next);
+					}
+				}
+			}
+				
+			isV = !isV;
+		}
+		
+		return new Pair<Integer>(-1, -1);
 	}
 
 	// do unit testing of this class
 	public static void main(String[] args) {
-		In in = new In(args[0]);
+		In in = new In("files/digraph1.txt");
 		Digraph G = new Digraph(in);
+		System.out.println(G);
+		
 		SAP sap = new SAP(G);
-		while (!StdIn.isEmpty()) {
-			int v = StdIn.readInt();
-			int w = StdIn.readInt();
-			int length = sap.length(v, w);
-			int ancestor = sap.ancestor(v, w);
+		List<Pair<Integer>> pairs = new ArrayList<>();
+		pairs.add(new Pair<>(3, 11));
+		pairs.add(new Pair<>(9, 12));
+		pairs.add(new Pair<>(7, 2));
+		pairs.add(new Pair<>(1, 6));
+		pairs.add(new Pair<>(0, 0));
+		
+		for(Pair<Integer> pair : pairs) {
+			int length = sap.length(pair.getV(), pair.getW());
+			int ancestor = sap.ancestor(pair.getV(), pair.getW());
 			StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+		}
+	}
+	
+	private static class Pair<T> {
+		private T v;
+		private T w;
+		
+		public Pair(T v, T w) {
+			this.v = v;
+			this.w = w;
+		}
+		
+		public T getV() {
+			return v;
+		}
+		
+		public T getW() {
+			return w;
 		}
 	}
 }
--- WordNet/src/WordNet.java
+++ WordNet/src/WordNet.java
@@ -1,33 +1,111 @@
-public class WordNet {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+
+public class WordNet {
+	private Map<String, Set<Integer>> nouneMap;
+	private Map<Integer, String> idMap;
+	private Digraph G;
+	private SAP sap;
+	private int maxId;
+	
 	// constructor takes the name of the two input files
 	public WordNet(String synsets, String hypernyms) {
+		In synsetsIn = new In(synsets);
+		In hypernymsIn = new In(hypernyms);
+		
+		maxId = 0;
+		populateMap(synsetsIn);
+		
+		populateDigraph(hypernymsIn);
 	}
 
+	private void populateMap(In synsetsIn) {
+		nouneMap = new HashMap<>();
+		idMap = new HashMap<>();
+		
+		String line;
+		while((line = synsetsIn.readLine()) != null) {
+			String[] words = line.split(",");
+			
+			int id = Integer.parseInt(words[0]);
+			if(id > maxId) maxId = id;
+			
+			idMap.put(id, words[1]);
+			String[] nouns = words[1].split("_");
+			
+			for(String noun : nouns) {
+				Set<Integer> ids;
+				
+				if(nouneMap.containsKey(noun)) {
+					ids = nouneMap.get(noun);
+				} else {
+					ids = new HashSet<>();
+				}
+				
+				ids.add(id);
+				nouneMap.put(noun, ids);
+			}
+		}
+	}
+	
+	private void populateDigraph(In hypernymsIn) {
+		G = new  Digraph(maxId + 1);
+		
+		String line;
+		while((line = hypernymsIn.readLine()) != null) {
+			String[] words = line.split(",");
+			
+			int from = Integer.parseInt(words[0]);
+			for(int i=1; i<words.length; i++) {
+				G.addEdge(from, Integer.parseInt(words[i]));
+			}
+		}
+		
+		sap = new SAP(G);
+	}
+	
 	// returns all WordNet nouns
 	public Iterable<String> nouns() {
-		return null;
+		return nouneMap.keySet();
 	}
 
 	// is the word a WordNet noun?
 	public boolean isNoun(String word) {
-		return false;
+		return nouneMap.containsKey(word);
 	}
 
 	// distance between nounA and nounB (defined below)
 	public int distance(String nounA, String nounB) {
-		return 0;
+		return sap.length(nouneMap.get(nounA), nouneMap.get(nounB));
 	}
 
 	// a synset (second field of synsets.txt) that is the common ancestor of
 	// nounA and nounB
 	// in a shortest ancestral path (defined below)
 	public String sap(String nounA, String nounB) {
-		return null;
+		int sapId = sap.ancestor(nouneMap.get(nounA), nouneMap.get(nounB));
+		if (sapId == -1) return null;
+		else return idMap.get(sapId);
 	}
 
 	// do unit testing of this class
 	public static void main(String[] args) {
-
+		WordNet wordNet = new WordNet("files/synsets15.txt", "files/hypernyms15Tree.txt");
+		
+		System.out.println(wordNet.nouns());
+		
+		System.out.println(wordNet.isNoun("a"));
+		System.out.println(wordNet.isNoun("z"));
+		
+		System.out.println(wordNet.distance("a", "o"));
+		System.out.println(wordNet.sap("a", "o"));
+		
+		System.out.println(wordNet.distance("a", "a"));
+		System.out.println(wordNet.sap("a", "a"));
 	}
 }
